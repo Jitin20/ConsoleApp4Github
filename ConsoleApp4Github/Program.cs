c57aaff7e4690aa4e0aee6b7395aa79342a73d7d
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-11 09:36:37 +0530

    Add project files.

---
+++ ConsoleApp4Github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp4Github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
